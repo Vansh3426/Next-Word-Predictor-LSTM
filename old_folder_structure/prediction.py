3a89a68d0b5e724b77d72dad0db92eb51038d5b5
Vansh Mhatre <vanshmhatre342006@gmail.com>
2026-01-23 23:05:54 +0530

    fix : olde folder structure

---
+++ old_folder_structure/prediction.py
@@ -0,0 +1,95 @@
+import torch
+from LSTM.old_folder_structure.model import Next_word_predictor
+import spacy
+import json
+import os
+
+# Get the directory of the current file (run_prediction.py)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Build the path to vocab.json in the same folder
+VOCAB_PATH = os.path.join(BASE_DIR, "vocab.json")
+
+# Load the vocab
+with open(VOCAB_PATH, "r", encoding="utf-8") as f:
+    vocab = json.load(f)
+vocab_size =len(vocab)
+
+text ="Let me see,” said Holmes,"
+
+
+model = Next_word_predictor(vocab_size)
+model.load_state_dict(torch.load('LSTM/next_word_prediction_model.pth'))
+model.eval()
+
+
+
+nlp = spacy.load("en_core_web_md")
+
+def tokenize(text):
+    doc =nlp(text)
+    
+    tokenized_text =[]
+    for token in doc:
+        
+        
+        # token = token.split(" ")
+        if not token.is_punct and not token.is_quote and not token.is_space:
+            tokenized_text.append(token.lower_)
+    return tokenized_text
+
+tokens =tokenize(text)
+
+def text_to_index_new(sentence):
+    
+    tokenize_sentence_to_index =[]
+    for token in sentence:
+        if token  in vocab:
+            token = vocab[token]
+            tokenize_sentence_to_index.append(token)
+
+    return tokenize_sentence_to_index
+
+def padding(max_length,sequence):
+    
+    padded_sequences = []
+    max_size = max_length
+    padded_sequences.append([0]*((max_size)-len(sequence)) + sequence)
+    
+    return padded_sequences
+
+max_size_of_input_sequence = 65
+
+def prediction(text ,model):
+    
+    tokenized_text =tokenize(text)
+    # print(tokenized_text)
+    
+    
+    sent_index =text_to_index_new(tokenized_text)
+    # print(sent_index)
+    
+    
+    padded_sent =padding(max_size_of_input_sequence ,sent_index)
+    
+    padded_sent =torch.tensor(padded_sent ,dtype=torch.long)
+    # print(padded_sent.shape)
+    
+    logits = model(padded_sent)
+    
+    # print(logits.shape)
+    output =torch.softmax(logits ,dim=1)
+    value ,index = torch.max(output,dim=1)
+    
+    return text + ' ' +list(vocab.keys())[index]
+    
+    
+    
+
+
+for i in range(10):
+    output =prediction(text,model)
+    
+    print(output)
+    
+    text = output
